83165ed7ffd2e487d11dc7a84bdb15ddc2eb04c9
YOUDONGJUN <ehdwnsdb@naver.com>
2023-02-01 16:47:55 +0900

    230201 16:45

---
+++ src/main/java/dbFilm/dbFilmMain.java
@@ -0,0 +1,28 @@
+package dbFilm;
+
+import dbConn.ConnectionMaker;
+import dbConn.MySqlConnectionMaker;
+import dbFilm.viewer.CustomerViewer;
+import dbFilm.viewer.StaffViewer;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class dbFilmMain {
+
+
+    public static void main(String[] args) {
+
+        Scanner SCANNER = new Scanner(System.in);
+        Connection connection = null;
+//        StaffDTO logIn = new StaffDTO();
+
+        ConnectionMaker connectionMaker = new MySqlConnectionMaker();
+        CustomerViewer customerViewer = new CustomerViewer(connectionMaker);
+        StaffViewer staffViewer = new StaffViewer(SCANNER,connection);
+//        FilmViewer filmViewer = new FilmViewer(SCANNER, connection, logIn);
+        customerViewer.showIndex();
+
+    }
+
+}
